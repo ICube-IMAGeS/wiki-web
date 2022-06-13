de9a6db4e4e8dc8bcab81e714083aca7867cd70f
Luc Vedrenne <vedrenneluc@gmail.com>
2022-06-13 18:13:41 +0200

    python code working; now trying to access python lib

--- .github/inject.py
+++ .github/inject.py
@@ -1,15 +1,21 @@
+import os
 import argparse
+from pathlib import Path
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser('Injector')
-    parser.add_argument('--path', type=str)
+    parser.add_argument('--secret', type=str)
     return parser.parse_args()
 
 
-def inject(path: str) -> None:
-    print(path)
+def inject(secret: str) -> None:
+    python_path = os.popen('which python').stream().strip()
+    lib_path = Path(python_path).resolve().parent / 'lib'
+    print('Python bin path : ', python_path)
+    print('Python lib path : ', lib_path)
+    print('secret : ', secret)
 
 
 if __name__ == '__main__':
-    inject(parse_args().path)
+    inject(parse_args().secret)
